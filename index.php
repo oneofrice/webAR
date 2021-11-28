e3ce1021d9a08b1ee8e03b3833c659243e530cbf
Anton Abramov <abramov@intervolga.ru>
2021-11-28 14:07:04 +0300

    [AR-test-3] - переписывание на php и создание фронта

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+require_once "logic.php";
+?>
+
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <link rel="stylesheet" type="text/css" href="style.css"/>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="style.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <title>WebAR - научная работа</title>
+</head>
+<body>
+<header>
+    <div class="container top_title">
+        <h1>Выберете модель для просмотра</h1>
+    </div>
+</header>
+<div class="container table_content">
+    <div class="row">
+        <div class="col-sm">
+            Одна из трёх колонок
+        </div>
+        <div class="col-sm">
+            Одна из трёх колонок
+        </div>
+        <div class="col-sm">
+            Одна из трёх колонок
+        </div>
+    </div>
+</div>
+</body>
+</html>
